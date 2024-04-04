a602c6c0a94a8fd51ca568f92c6f79e494b88f07
Keyal <1920194156@qq.com>
2024-04-04 08:09:07 +0800

    I

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ if __name__ == '__main__':
                                      target_size=(128, 128))
         modelUnet = unet(learningRate=learning_rateI, decayRate=decayI, input_size=(windowLen, windowLen, 1))
         model_checkpoint = ModelCheckpoint(save_model_path,
-                                           monitor='val_jaccard_loss', mode='min',
+                                           mode='min',
                                            verbose=1, save_best_only=True, save_freq=NumEpochEval)
         history1 = modelUnet.fit(trainGener, epochs=NumEpochs,
                                  steps_per_epoch=int(n_imagesTrain / batch_size),
