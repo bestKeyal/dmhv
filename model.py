cbebf5638738d1afa08db57d765d54763098c50a
Keyal <1920194156@qq.com>
2024-04-07 11:35:32 +0800

    a

--- model.py
+++ model.py
@@ -2,13 +2,53 @@ from tensorflow.keras.models import *
 from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import *
 from tensorflow.keras import backend as K
+from tensorflow import keras
+import tensorflow as tf
 
-#The U-Net model
-#This code was original downloaded from https://github.com/zhixuhao/unet, then edited
+
+def dice_loss(y_true, y_pred, smooth=1e-6):
+    y_true_f = tf.reshape(tf.cast(y_true, tf.float32), [-1])
+    y_pred_f = tf.reshape(tf.cast(y_pred, tf.float32), [-1])
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + smooth)
+    return 1 - score
+
+def bce_dice_loss(y_true, y_pred):
+    bce = tf.keras.losses.binary_crossentropy(y_true, y_pred, label_smoothing=1e-2)
+    dice = dice_loss(y_true, y_pred)
+    return bce + dice
+
+def iou_coefficient(y_true, y_pred, smooth=1e-6):
+    y_true_f = tf.cast(tf.reshape(y_true, [-1]), tf.float32)
+    y_pred_f = tf.cast(tf.reshape(y_pred, [-1]), tf.float32)
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    union = tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) - intersection
+    iou = (intersection + smooth) / (union + smooth)
+    return iou
+
+
+def r_squared(y_true, y_pred):
+    """
+    计算决定系数R^2，用作Keras模型的评估指标。
+
+    :param y_true: 真实值
+    :param y_pred: 预测值
+    :return: R^2的值
+    """
+    # 总平方和（Total Sum of Squares, SSE）
+    ss_total = tf.reduce_sum(tf.square(y_true - tf.reduce_mean(y_true)))
+    # 回归平方和（Residual Sum of Squares, SSR）
+    ss_res = tf.reduce_sum(tf.square(y_true - y_pred))
+    # R^2 计算
+    r2 = 1 - ss_res/ss_total
+    return r2
+
+
+
 def jaccard_loss(y_true, y_pred): #https://www.jeremyjordan.me/evaluating-image-segmentation-models/
     intersection = y_true*y_pred
     union = 1 -((1-y_true)*(1-y_pred))
-    return 1-(K.sum(intersection) / K.sum(union))
+    return 1-(tf.reduce_sum(intersection) / tf.reduce_sum(union))
 
 def unet(pretrained_weights = None,input_size = (128,128,1),learningRate=1e-5,decayRate=1e-7):
     ModInputs = Input(input_size)
@@ -54,8 +94,9 @@ def unet(pretrained_weights = None,input_size = (128,128,1),learningRate=1e-5,de
 
     model = Model(inputs = ModInputs, outputs = conv10)
 
-    model.compile(optimizer=Adam(learning_rate=learningRate, decay=decayRate), loss='binary_crossentropy',
-                  metrics=['accuracy', jaccard_loss])
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-5),
+                  loss=bce_dice_loss,
+                  metrics=['accuracy', bce_dice_loss, dice_loss, iou_coefficient, r_squared])
 
     model.summary()
 
